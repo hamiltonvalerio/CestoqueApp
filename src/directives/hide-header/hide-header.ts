3629990536f2a6ff4f8c815a3c0873dcf0e50479
Hamilton <htecmac@gmail.com>
2020-10-02 12:32:39 -0300

    Scroll event

--- src/directives/hide-header/hide-header.ts
+++ src/directives/hide-header/hide-header.ts
@@ -16,30 +16,50 @@ export class HideHeaderDirective {
 
   private header: HTMLElement;
   private headerHeight: number;
-  private scroollContent: HTMLElement;
+  private fixedContent: HTMLElement;
+  private scrollContent: HTMLElement;
+  private transitionTime: string = "400";
 
   constructor(private element: ElementRef, private renderer: Renderer) {
-    
+    console.log(element);
+    console.log(renderer);
   }
 
   ngOnInit(){
-    this.scroollContent = this.element.nativeElement.getElementsByClassName("scrooll-content")[0];
+
+    this.scrollContent = this.element.nativeElement.getElementsByClassName("scroll-content")[0];
+    this.fixedContent = this.element.nativeElement.getElementsByClassName("fixed-content")[0];
+
+    this.renderer.setElementStyle(this.scrollContent, "transition", "margin-top "+ this.transitionTime);
+    this.renderer.setElementStyle(this.fixedContent, "transition", "margin-top "+ this.transitionTime);
+
   }
 
   onScrooll(event){
+
+    console.log(event);
+
     if(!this.header){
       this.header = event.headerElement;
       this.headerHeight = this.header.clientHeight;
-      console.log(this.headerHeight);
+
+      this.renderer.setElementStyle(this.header, "transition", "top "+ this.transitionTime);
+      //console.log(this.headerHeight);
     }
 
-    if(event.scrollTop > 0){
-      this.renderer.setElementStyle(this.header, "top", "-" + this.headerHeight + "px");
-      this.renderer.setElementStyle(this.scroollContent, "margin-top", "0px");
-    }else{
-      this.renderer.setElementStyle(this.header, "top", "0px");
-      this.renderer.setElementStyle(this.scroollContent, "margin-top", "-" + this.headerHeight + "px");
-    }
+    event.domWrite(() => {
+      console.log(event.directionY);
+      if(event.directionY == "down"){
+        this.renderer.setElementStyle(this.header, "top", "-" + this.headerHeight + "px");
+        this.renderer.setElementStyle(this.scrollContent, "margin-top", "0px");
+        this.renderer.setElementStyle(this.fixedContent, "margin-top", "0px");
+      }else{
+        this.renderer.setElementStyle(this.header, "top", "0px");
+        this.renderer.setElementStyle(this.scrollContent, "margin-top", this.headerHeight + "px");
+        this.renderer.setElementStyle(this.fixedContent, "margin-top", this.headerHeight + "px");
+      }
+
+    });
   }
 
 }
